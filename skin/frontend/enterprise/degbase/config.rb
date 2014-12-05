c45ba0ca23c796b96a3d77870d695bfb3c40f464
Joseph Lawhorn <jlawhorn@degdigital.com>
2014-12-05 12:39:12 -0600

    Included autoprefixer in rubyconfig

--- skin/frontend/enterprise/degbase/config.rb
+++ skin/frontend/enterprise/degbase/config.rb
@@ -4,3 +4,13 @@ sass_dir = "css"
 images_dir = "images"
 javascripts_dir = "js"
 output_style = :nested
+sourcemap = true
+
+require 'autoprefixer-rails'
+
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  File.open(file, 'w') do |io|
+    io << AutoprefixerRails.process(css, browsers: ['> 5%', 'last 2 versions', 'Android 2.3'])
+  end
+end
